0d8c31651c05b93c1f8886ea5e305f00246bf422
Shaishav Kr <shaishav.kumar@rapyuta-robotics.com>
2024-09-01 01:00:42 +0530

    updated config

--- config.lua
+++ config.lua
@@ -14,14 +14,25 @@ function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-lvim.plugins = {
-  { 'dnlhc/glance.nvim' },
-  { "bullets-vim/bullets.vim", },
+local ux_plugins = {
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+        hide_cursor = true,          -- Hide cursor while scrolling
+        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,       -- Default easing function
+        pre_hook = nil,              -- Function to run before the scrolling animation starts
+        post_hook = nil,             -- Function to run after the scrolling animation ends
+      })
+    end
   },
   {
     "hedyhli/outline.nvim",
@@ -34,63 +45,71 @@ lvim.plugins = {
 
     }
   },
+
+}
+
+
+local git_plugins = {
   {
-    "lukas-reineke/headlines.nvim",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = true, -- or `opts = {}`
-  },
-  {
-    "3rd/image.nvim",
-    event = "VeryLazy",
+    "NeogitOrg/neogit",
     dependencies = {
-      "leafo/magick",
-      {
-        "vhyrro/luarocks.nvim",
-        opts = {
-          rocks = {
-            hererocks = true,
-          },
-        },
-      },
-      {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-          require("nvim-treesitter.configs").setup({
-            ensure_installed = { "markdown" },
-            highlight = { enable = true },
-          })
-        end,
-      },
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
     },
-    opts = {
-      backend = "kitty",
-      integrations = {
-        markdown = {
-          enabled = true,
-          clear_in_insert_mode = false,
-          download_remote_images = true,
-          only_render_image_at_cursor = true,
-          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-        },
-        html = {
-          enabled = true,
-        },
-        css = {
-          enabled = true,
-        },
-      },
-      max_width = nil,
-      max_height = nil,
-      max_width_window_percentage = nil,
-      max_height_window_percentage = 50,
-      kitty_method = "normal",
-      window_overlap_clear_enabled = false,
-      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-      editor_only_render_when_focused = true,
-      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
-    },
+    config = true
   },
+  { 'ThePrimeagen/git-worktree.nvim' },
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead",
+  },
+
+
+  {
+    "ruifm/gitlinker.nvim",
+    event = "BufRead",
+    config = function()
+      require("gitlinker").setup {
+        opts = {
+          -- remote = 'github', -- force the use of a specific remote
+          -- adds current line nr in the url for normal mode
+          add_current_line_on_normal_mode = true,
+          -- callback for what to do with the url
+          action_callback = require("gitlinker.actions").open_in_browser,
+          -- print the url after performing the action
+          print_url = false,
+          -- mapping to call url generation
+          mappings = "<leader>gy",
+        },
+      }
+    end,
+    dependencies = "nvim-lua/plenary.nvim",
+  },
+
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require("octo").setup({
+        suppress_missing_scope = {
+          projects_v2 = true,
+        }
+      })
+    end,
+  },
+}
+
+local dev_helper_plugins = {
+
+  { 'dnlhc/glance.nvim' },
   {
     "prettier/vim-prettier",
     -- File types to enable vim-prettier for
@@ -106,35 +125,6 @@ lvim.plugins = {
       -- "always" is what enables auto wrapping
       vim.g["prettier#config#prose_wrap"] = "always"
     end,
-  },
-  {
-    "HakonHarnes/img-clip.nvim",
-    event = "VeryLazy",
-    opts = {
-      default = {
-        dir_path = "Assets",
-        use_absolute_path = false,
-        relative_to_current_file = true,
-        extensions = "png",
-        process_cmd =
-        "convert - -depth 24 -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 png:-",
-
-
-      },
-      -- add options here
-      -- or leave it empty to use the default settings
-      filetypes = {
-        markdown = {
-          url_encode_path = true, ---@type boolean
-          template = "![$CURSOR]($FILE_PATH)", ---@type string
-          download_images = true, ---@type boolean
-        },
-      },
-    },
-    keys = {
-      -- suggested keymap
-      { "<leader>mp", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-    },
   },
   {
     "frankroeder/parrot.nvim",
@@ -191,7 +181,121 @@ lvim.plugins = {
       end,
     }
   }
+
 }
+
+local markdown_plugins = {
+  { "bullets-vim/bullets.vim", },
+  {
+    "lukas-reineke/headlines.nvim",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true, -- or `opts = {}`
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+    "3rd/image.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "leafo/magick",
+      {
+        "vhyrro/luarocks.nvim",
+        opts = {
+          rocks = {
+            hererocks = true,
+          },
+        },
+      },
+      {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+          require("nvim-treesitter.configs").setup({
+            ensure_installed = { "markdown" },
+            highlight = { enable = true },
+          })
+        end,
+      },
+    },
+    opts = {
+      backend = "kitty",
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = true,
+          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        },
+        html = {
+          enabled = true,
+        },
+        css = {
+          enabled = true,
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+      kitty_method = "normal",
+      window_overlap_clear_enabled = false,
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      editor_only_render_when_focused = true,
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
+    },
+  },
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      default = {
+        dir_path = "Assets",
+        use_absolute_path = false,
+        relative_to_current_file = true,
+        extensions = "png",
+        process_cmd =
+        "convert - -depth 24 -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 png:-",
+
+
+      },
+      -- add options here
+      -- or leave it empty to use the default settings
+      filetypes = {
+        markdown = {
+          url_encode_path = true, ---@type boolean
+          template = "![$CURSOR]($FILE_PATH)", ---@type string
+          download_images = true, ---@type boolean
+        },
+      },
+    },
+    keys = {
+      -- suggested keymap
+      { "<leader>mp", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
+  },
+
+}
+
+for _, value in pairs(ux_plugins) do
+  table.insert(lvim.plugins, value)
+end
+for _, value in pairs(git_plugins) do
+  table.insert(lvim.plugins, value)
+end
+for _, value in pairs(dev_helper_plugins) do
+  table.insert(lvim.plugins, value)
+end
+for _, value in pairs(markdown_plugins) do
+  table.insert(lvim.plugins, value)
+end
+
+
+
 
 local allmode = { "n", "v", "i", "c" }
 keymap.set(allmode, "<C-BS>", "<C-w>")
@@ -215,7 +319,7 @@ keymap.set('n', 'gM', '<CMD>Glance implementations<CR>')
 -- keymap.set(allmode, "<M-\\>", ":ToggleTerm direction=float<cr>") -- list current changes per file with diff preview ["gs" for git status]
 keymap.set(allmode, "<C-1>", ":PrtChatToggle vsplit<cr>") -- list current changes per file with diff preview ["gs" for git status]
 -- keymap.set(allmode, "<C-`>", ":ToggleTerm direction=float<cr>") -- list current changes per file with diff preview ["gs" for git status]
-lvim.builtin.terminal.open_mapping = "<c-`>"
+lvim.builtin.terminal.open_mapping = "<C-`>"
 
 
 keymap.set({ "n", "c", "v" }, '<Space>mm', '<CMD>MarkdownPreview<CR>')
